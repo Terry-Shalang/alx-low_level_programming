4d63e738588c7437644bed954e8d7cd5da61ff6d
Terrence Shalang <64202786+Terry-Shalang@users.noreply.github.com>
2022-09-09 06:12:15 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -10,14 +10,14 @@
  *
  *   *
  *
- *   * Description: made  used printf(); function to print to output stream
+ *
  *
  *   * Return: returns 0 for success
  *
  *             */
 
 int main(void)
-
+/*Description: made  used printf(); function to print to output stream*/
 {
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
