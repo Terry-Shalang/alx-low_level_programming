6db90fe9eea88a181f4db6210378f0c020edccb3
Terrence Shalang <64202786+Terry-Shalang@users.noreply.github.com>
2022-09-09 06:20:14 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,12 +12,11 @@
 int main(void)
 /*Description : program uses size() to output the size of data types*/
 {
-
- printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(char));
- printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(int));
- printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(long int));
- printf("Size of a long long int: %d byte(s)\n", (unsigned long)sizeof(long long int));
- printf("Size of a float: %d byte(s)\n", (unsigned long)sizeof(float));
+	printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(char));
+ 	printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(int));
+ 	printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(long int));
+ 	printf("Size of a long long int: %d byte(s)\n", (unsigned long)sizeof(long long int));
+ 	printf("Size of a float: %d byte(s)\n", (unsigned long)sizeof(float));
 												
 	return (0);
 }
