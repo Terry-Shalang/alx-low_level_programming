0d13d641edfb80d98ab1ee56a1e8261ccb1a44bf
Terrence Shalang <64202786+Terry-Shalang@users.noreply.github.com>
2022-09-09 06:27:40 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,7 @@ int main(void)
 /*Description : program uses size() to output the size of data types*/
 {
 	long long int lli;
+
 	printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(char));
 	printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(long int));
