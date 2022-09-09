69ee0d49cd4f26da2141a92b6438289849afc469
Terrence Shalang <64202786+Terry-Shalang@users.noreply.github.com>
2022-09-09 06:13:22 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,18 +3,16 @@
 
 
 /**
- *
  *   * main - Entry point of the program
  *
  *   * function takes no arguments
  *
- *   *
  *
  *
  *
- *   * Return: returns 0 for success
  *
- *             */
+ *   * Return: returns 0 for success             
+ */
 
 int main(void)
 /*Description: made  used printf(); function to print to output stream*/
