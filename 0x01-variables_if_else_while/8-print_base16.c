b7f0144a3c87ae471eeab48b0acffeac569ffb94
Terrence Shalang <64202786+Terry-Shalang@users.noreply.github.com>
2022-09-12 22:28:41 +0200

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,7 +8,7 @@
 
   *
 
-  * Takes no arguments
+  * No arguments
 
   *
 
@@ -20,30 +20,30 @@ int main(void)
 
 {
 
-	char nbase_16;
+char nbase_16;
 
 
 
-	for (nbase_16 = 48; nbase_16 <= 57; nbase_16++)
+for (nbase_16 = 48; nbase_16 <= 57; nbase_16++)
 
-	{
+{
 
-		putchar(nbase_16);
+putchar(nbase_16);
 
-	}
+}
 
-	for (nbase_16 = 97; nbase_16 <= 102; nbase_16++)
+for (nbase_16 = 97; nbase_16 <= 102; nbase_16++)
 
-	{
+{
 
-		putchar(nbase_16);
+putchar(nbase_16);
 
-	}
+}
 
-	putchar('\n');
+putchar('\n');
 
 
 
-	return (0);
+return (0);
 
 }
