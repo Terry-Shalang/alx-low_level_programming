013d78a3a7009031410fbec9d5af5dc15e765b32
Terrence <terrence.shalang@hotmail.com>
2022-09-14 19:57:04 -0700

    functions and nested loops task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program entry
+ *
+ * return - always return 0 for success run
+ * */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
