972a4591693aeca9397af88e323c243a14001a54
Terry_SHA <terrence.shalang@gmail.com>
2023-10-07 09:07:22 +0200

    malloc 0 - bug fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,12 +10,12 @@
  */
 char *create_array(unsigned int size, char c)
 {
+    char * prt;
+    
     if (size == 0)
     {
         return NULL;
     }
-
-    char * prt;
 
     prt = (char *)malloc(sizeof(char) * size);
 
