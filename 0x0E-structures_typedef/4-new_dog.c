2e95582b27b2729e992c3ced4988bcb5f552330a
Terry_SHA <terrence.shalang@gmail.com>
2023-10-09 23:18:19 +0200

    new file:   0x0E-structures_typedef/4-new_dog.c modified:   0x0E-structures_typedef/main.h

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include "dog.h"
+
+/**
+ * init_dog - function that initialize a variable of type struct dog
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ *
+ * Return: pointer to struct type dog_t
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+
+	return d;
+}
