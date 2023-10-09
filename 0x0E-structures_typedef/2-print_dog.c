f2038da42a1a403ae0de1e38e012b267f56954d8
Terry_SHA <terrence.shalang@gmail.com>
2023-10-09 22:47:43 +0200

    new file:   0x0E-structures_typedef/2-print_dog.c modified:   0x0E-structures_typedef/main.h

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+#include "dog.h"
+
+/**
+ * print_dog - function that initialize a variable of type struct dog
+ * @d: pointer of type struct dog
+ *
+ * Return: void (nothing)
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		d->name == NULL ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
+		d->age < 0.0 ? printf("Age: (nil)\n") : printf("Age: %.1f\n", d->age);
+		d->owner == NULL ? printf("Owner: (nil)\n") : printf("Owner: %s\n", d->owner);
+	}
+}
