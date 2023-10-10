eccfb5cc7477724b18a21d2d398bded0e174881d
Terry_SHA <terrence.shalang@gmail.com>
2023-10-10 09:03:05 +0200

    modified:   0x0D-preprocessor/0-object_like_macro.h modified:   0x0D-preprocessor/1-pi.h modified:   0x0D-preprocessor/2-main.c modified:   0x0D-preprocessor/3-function_like_macro.h modified:   0x0D-preprocessor/4-sum.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,8 +3,10 @@
 
 /**
  * File: 3-function_like_macro.h
- * 
- * Desc:  function-like macro ABS(x) that computes the absolute value of a number x
+ * @x: number
+ *
+ * Desc:  function-like macro ABS(x) that computes the absolute
+ * value of a number x
 */
 
 #define ABS(x) ((x) < (0) ? (x) * (-1) : (x))
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -3,7 +3,9 @@
 
 /**
  * File: 4-sum.h
- * 
+ * @x: number 1
+ * @y: number 2
+ *
  * Desc:   function-like macro SUM(x, y) that computes the sum of the numbers x and y
 */
 
