6eb0f5c5f6c9c36fe18b48668a0832eae5e20b9d
Terry_SHA <terrence.shalang@gmail.com>
2023-10-10 09:04:46 +0200

    modified:   0x0D-preprocessor/4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -6,7 +6,8 @@
  * @x: number 1
  * @y: number 2
  *
- * Desc:   function-like macro SUM(x, y) that computes the sum of the numbers x and y
+ * Desc:   function-like macro SUM(x, y) that computes
+ * the sum of the numbers x and y
 */
 
 #define SUM(x, y) ((x) + (y))
