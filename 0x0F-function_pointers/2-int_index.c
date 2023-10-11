5b2e060fccd73c9fbb3c4084b872caf1bd814234
Terry_SHA <terrence.shalang@gmail.com>
2023-10-12 01:03:36 +0200

    modified:   0x0F-function_pointers/1-array_iterator.c modified:   0x0F-function_pointers/2-int_index.c modified:   0x0F-function_pointers/function_pointers.h

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,7 +7,7 @@
  * @size: size of the array
  * @cmp: Pointer to action function
  *
- * Return: The index of the first element for which the cmp 
+ * Return: The index of the first element for which the cmp
  * function does not return 0.If no element matches, return -1
  * If size <= 0, return -1
 */
@@ -17,6 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size > 0)
 	{
 		int i = 0;
+        
 		while (i < size)
 		{
 			if (cmp(array[i]) != 0)
