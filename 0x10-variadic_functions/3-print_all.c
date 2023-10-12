0aa8876dd3e82379d8cab50dfe8754aeffbe624a
Terry_SHA <terrence.shalang@gmail.com>
2023-10-12 22:53:50 +0200

    new file:   0x10-variadic_functions/0-sum_them_all.c new file:   0x10-variadic_functions/1-print_numbers.c new file:   0x10-variadic_functions/2-print_strings.c new file:   0x10-variadic_functions/3-print_all.c new file:   0x10-variadic_functions/README.md new file:   0x10-variadic_functions/variadic_functions.h

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - function that prints anything.
+ * @format: a list of types of arguments passed.
+ *
+ * Return: void(nothing).
+ */
+void print_all(const char * const format, ...)
+{
+
+}
