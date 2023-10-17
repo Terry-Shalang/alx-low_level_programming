5ada8c18d049f31296dc723696e07b6216f452c6
Terry_SHA <terrence.shalang@gmail.com>
2023-10-17 07:44:19 +0200

    new file:   0x0A-argc_argv/0-whatsmyname.c new file:   0x0A-argc_argv/1-args.c new file:   0x0A-argc_argv/2-args.c new file:   0x0A-argc_argv/3-mul.c new file:   0x0A-argc_argv/4-add.c new file:   0x0A-argc_argv/README.md new file:   0x0A-argc_argv/main.h

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ *
+ * @argc: count of arguments passed
+ * @argv: array of strings (arguments)
+ *
+ * Return: 0 for success
+*/
+int main(int argc, char *argv[])
+{   
+	if (argc == 2)
+	{
+		printf("%d", atoi(argv[1]) * atoi(argv[2]));
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - multiplies two numbers
+ *
+ * @argc: count of arguments passed
+ * @argv: array of strings (arguments)
+ *
+ * Return: 1 if a non-integer is passed in arguments 0 otherwise
+*/
+int main(int argc, char *argv[])
+{   
+	int i, sum;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += atoi(argv[i]);
+		}
+
+	}
+	
+	return (0);
+}
